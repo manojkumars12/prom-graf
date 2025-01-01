9c2e9872668f556486ae8703b3f76ffd41e9dd4e
Manoj <manojskumar3122@gmail.com>
2025-01-01 11:44:09 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/user", (req, res) => {
+    res.send({
+        name: "John Doe",
+        age: 25,
+    });
+});
+
+app.post("/user", (req, res) => {
+    const user = req.body;
+    res.send({
+        ...user,
+        id: 1,
+    });
+});
+
+app.listen(3000);
